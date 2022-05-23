6cc27e3d2dacd8c36c0811c49e9f8cbca2d3aa77
Ganesh9392 <81861545+Ganesh9392@users.noreply.github.com>
2022-05-23 11:20:16 +0530

    num=int(input()) l=list(map(int,input().split())) n1=num//2 l1=[] l2=[] for i in range(n1):     l1.append(l[i]) for j in range(n1,num):     l2.append(l[j]) print(sum(l1)) print(sum(l2))

---
+++ sum_of_first_half_and_second_half_in_an_array.py
@@ -0,0 +1,11 @@
+num=int(input())
+l=list(map(int,input().split()))
+n1=num//2
+l1=[]
+l2=[]
+for i in range(n1):
+    l1.append(l[i])
+for j in range(n1,num):
+    l2.append(l[j])
+print(sum(l1))
+print(sum(l2))
