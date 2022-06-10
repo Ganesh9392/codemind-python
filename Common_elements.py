6205b3f2460b96acc21c9f86d3eeb13e3a75c1a4
Ganesh9392 <81861545+Ganesh9392@users.noreply.github.com>
2022-06-10 14:20:41 +0530

    a,b=map(int,input().split()) l1=list(map(int,input().split())) l2=list(map(int,input().split())) l3=[] for i in range(len(l1)):     if(l1[i] in l2):         if(l1[i] not in l3):             l3.append(l1[i]) print(*l3)

---
+++ Common_elements.py
@@ -0,0 +1,9 @@
+a,b=map(int,input().split())
+l1=list(map(int,input().split()))
+l2=list(map(int,input().split()))
+l3=[]
+for i in range(len(l1)):
+    if(l1[i] in l2):
+        if(l1[i] not in l3):
+            l3.append(l1[i])
+print(*l3)
