e39a1170e48b4c7c21dee0f62ee0567cf7b76ee1
Ganesh9392 <81861545+Ganesh9392@users.noreply.github.com>
2022-06-11 15:05:29 +0530

    s1=input() s1=s1.lower() s1=list(s1) s2=input() s2=s2.lower() s2=list(s2) l=[] for i in range(len(s2)):     if(s2[i] in s1):         if(s2[i] not in l):             if(s2[i]!=" "):                 l.append(s2[i]) for j in range(len(s1)):     if(s1[j]  in s2):         if(s1[j] not in l):             if(s1[j]!=" "):                 l.append(s1[j]) print(len(l))

---
+++ common_characters_-II.py
@@ -0,0 +1,18 @@
+s1=input()
+s1=s1.lower()
+s1=list(s1)
+s2=input()
+s2=s2.lower()
+s2=list(s2)
+l=[]
+for i in range(len(s2)):
+    if(s2[i] in s1):
+        if(s2[i] not in l):
+            if(s2[i]!=" "):
+                l.append(s2[i])
+for j in range(len(s1)):
+    if(s1[j]  in s2):
+        if(s1[j] not in l):
+            if(s1[j]!=" "):
+                l.append(s1[j])
+print(len(l))
