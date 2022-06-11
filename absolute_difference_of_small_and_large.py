c58b74e25019286005388889412adf1bbb935f8b
Ganesh9392 <81861545+Ganesh9392@users.noreply.github.com>
2022-06-11 13:28:29 +0530

    n=input() l=list(n.split(" ")) ls=0 ss=0 for i in range(len(l)):     ls=0     ss=0     l2=[]     s=l[i]     for j in range(len(s)):         l2.append(ord(s[j]))     ls=max(l2)     ss=min(l2)     print(ls-ss,end=" ")

---
+++ absolute_difference_of_small_and_large.py
@@ -0,0 +1,15 @@
+n=input()
+l=list(n.split(" "))
+ls=0
+ss=0
+for i in range(len(l)):
+    ls=0
+    ss=0
+    l2=[]
+    s=l[i]
+    for j in range(len(s)):
+        l2.append(ord(s[j]))
+    ls=max(l2)
+    ss=min(l2)
+    print(ls-ss,end=" ")
+        
