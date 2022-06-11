db515d9fb95b3499da2bb3af246995d659e81a51
Ganesh9392 <81861545+Ganesh9392@users.noreply.github.com>
2022-06-11 14:19:05 +0530

    n=input() n=list(n) l=[] c=0 for i in range(len(n)):     if n[i]!=" ":         if(n[i] not in l):             if(n[i]>"Z"):                 l.append(n[i]) l.sort() for i in range(len(l)):     print(l[i],end="")

--- Distinct_characters.py
+++ Distinct_characters.py
@@ -1,10 +1,12 @@
 n=input()
 n=list(n)
 l=[]
+c=0
 for i in range(len(n)):
-    if(n.count(n[i])==1):
-        if(ord(n[i])>ord("Z")):
-            l.append(n[i])
+    if n[i]!=" ":
+        if(n[i] not in l):
+            if(n[i]>"Z"):
+                l.append(n[i])
 l.sort()
 for i in range(len(l)):
     print(l[i],end="")
