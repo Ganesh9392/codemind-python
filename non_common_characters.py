240b062bc318249b033f6962a4b88d912254a4b6
Ganesh9392 <81861545+Ganesh9392@users.noreply.github.com>
2022-06-11 14:56:58 +0530

    s1=input() s1=s1.lower() s1=list(s1) s2=input() s2=s2.lower() s2=list(s2) l=[] for i in range(len(s2)):     if(s2[i] not in s1):         if(s2[i] not in l):             if(s2[i]!=" "):                 l.append(s2[i]) for j in range(len(s1)):     if(s1[j] not in s2):         if(s1[j] not in l):             if(s1[j]!=" "):                 l.append(s1[j]) l.sort() for i in range(len(l)):     print(l[i],end="")

---
+++ non_common_characters.py
@@ -0,0 +1,20 @@
+s1=input()
+s1=s1.lower()
+s1=list(s1)
+s2=input()
+s2=s2.lower()
+s2=list(s2)
+l=[]
+for i in range(len(s2)):
+    if(s2[i] not in s1):
+        if(s2[i] not in l):
+            if(s2[i]!=" "):
+                l.append(s2[i])
+for j in range(len(s1)):
+    if(s1[j] not in s2):
+        if(s1[j] not in l):
+            if(s1[j]!=" "):
+                l.append(s1[j])
+l.sort()
+for i in range(len(l)):
+    print(l[i],end="")
