f9d6d1b4d79451ab62ffa5a24bd3beadd037ca0e
Ganesh9392 <81861545+Ganesh9392@users.noreply.github.com>
2022-09-18 14:29:33 +0530

    n=int(input()) l="ABCDEFGHIJKLMNOPQRSTUVWXYZ" l=list(l) for i in range(n,0,-1):     for _ in range(i):         print(l[i-1],end=" ")     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,5 +1,7 @@
 n=int(input())
-for i in range(1,n+1):
-    for j in range(1,i+1):
-        print(j,end="")
+l="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+l=list(l)
+for i in range(n,0,-1):
+    for _ in range(i):
+        print(l[i-1],end=" ")
     print()
